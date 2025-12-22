4bc9d5f647664184d2b7f3053548162af4b0b58c
anaitullah31 <anaitullah198@gmail.com>
2025-12-22 17:46:40 +0800

    Practice Access modifier

--- module-3/index.ts
+++ module-3/index.ts
@@ -80,83 +80,110 @@ const add = (
   param2: number | string
 ): string | number => {
   if (typeof param1 === "number" && typeof param2 === "number") {
-    return param1 + param2
+    return param1 + param2;
   } else {
-    return param1.toString() + param2.toString()
+    return param1.toString() + param2.toString();
   }
 };
-const result1 = add(4, 5)
-const result2 = add("4", "5")
-console.log(result1, result2)
+const result1 = add(4, 5);
+const result2 = add("4", "5");
+console.log(result1, result2);
 
 // In guard
 type NormalUser = {
-    name: string
-}
+  name: string;
+};
 type AdminUser = {
-    name: string;
-    role: 'admin'
-}
+  name: string;
+  role: "admin";
+};
 const getUser = (user: NormalUser | AdminUser) => {
-    if('role' in user) {
-        console.log(`I am the ${user.role}`)
-    } else {
-        console.log(`I am ${user.name}`)
-    }
-}
+  if ("role" in user) {
+    console.log(`I am the ${user.role}`);
+  } else {
+    console.log(`I am ${user.name}`);
+  }
+};
 const normalUser: NormalUser = {
-    name: "Mr. User"
-}
+  name: "Mr. User",
+};
 const adminlUser: AdminUser = {
-    name: "Mr. Admin",
-    role: 'admin'
-}
-getUser(normalUser)
-getUser(adminlUser)
+  name: "Mr. Admin",
+  role: "admin",
+};
+getUser(normalUser);
+getUser(adminlUser);
 
 // Type guard using Instance of
 class Animal1 {
-    name: string;
-    species: string;
-    constructor(name: string, species: string){
-        this.name = name;
-        this.species = species
-    }
-    makeSound() {
-        console.log(`${this.name} is making sound`)
-    }
+  name: string;
+  species: string;
+  constructor(name: string, species: string) {
+    this.name = name;
+    this.species = species;
+  }
+  makeSound() {
+    console.log(`${this.name} is making sound`);
+  }
 }
 class Dog1 extends Animal1 {
-    constructor(name: string, species: string){
-        super(name, species)
-    }
-    makeBark() {
-        console.log("I am barking")
-    }
+  constructor(name: string, species: string) {
+    super(name, species);
+  }
+  makeBark() {
+    console.log("I am barking");
+  }
 }
 class Cat1 extends Animal1 {
-    constructor(name: string, species: string){
-        super(name, species)
-    }
-    makeMew() {
-        console.log("I am mewaing")
-    }
+  constructor(name: string, species: string) {
+    super(name, species);
+  }
+  makeMew() {
+    console.log("I am mewaing");
+  }
 }
 const isDog1 = (animal: Animal1): animal is Dog1 => {
-    return animal instanceof Dog1
-}
+  return animal instanceof Dog1;
+};
 const isCat1 = (animal: Animal1): animal is Cat1 => {
-    return animal instanceof Cat1
-}
+  return animal instanceof Cat1;
+};
 const getAnimal = (animal: Animal) => {
-    if(animal instanceof Dog1) {
-       return animal.makeBark()
-    } else if(animal instanceof Cat1) {
-       return animal.makeMew()
-    } else {
-       return animal.makeSound()
-    }
+  if (animal instanceof Dog1) {
+    return animal.makeBark();
+  } else if (animal instanceof Cat1) {
+    return animal.makeMew();
+  } else {
+    return animal.makeSound();
+  }
+};
+
+const dog1 = new Dog1("Dog", "Doggggg");
+const cat1 = new Cat1("Cat", "Catttttt");
+
+// Access Modifier
+class BankAccount {
+  readonly id: number;
+  public name: string;
+  private _balance: number;
+  protected _balance1: number;
+  constructor(id: number, name: string, balance: number, _balance1: number) {
+    this.id = id;
+    this.name = name;
+    this._balance = balance;
+    this._balance1 = _balance1
+  }
+  public addMoney(amount: number) {
+    return this._balance + amount;
+  }
+  public getBalance() {
+    return this._balance;
+  }
 }
 
-const dog1 = new Dog1('Dog', 'Doggggg')
-const cat1 = new Cat1('Cat', 'Catttttt')
+class StudentAccount extends BankAccount {
+
+}
+const poorPeopleAccount = new BankAccount(1, "Mr. Poor", 20, 52);
+const newBal = poorPeopleAccount.addMoney(50);
+const newBal1 = poorPeopleAccount.getBalance();
